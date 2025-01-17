b41677bd53b859e744c5e3fa30c8e87cf651dbba
OmeshSachethana <sachethana12@gmail.com>
2025-01-17 20:43:38 +0530

    fixed issues

--- app/app.js
+++ app/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const path = require('path');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
 
 const app = express();
+
+// Serve static files from the 'public' folder
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Middleware for parsing cookies and request bodies
 app.use(cookieParser());
